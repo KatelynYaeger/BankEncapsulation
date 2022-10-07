5c52ddd3facef89d89a6ad32d3dcdd0e4546a578
Katelyn Yaeger <knyaeger93@gmail.com>
2022-10-07 17:56:19 -0500

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newAccount = new BankAccount();
+
+            Console.WriteLine("Hello there! How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            newAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now ${newAccount.GetBalance()}");
+
         }
     }
 }
